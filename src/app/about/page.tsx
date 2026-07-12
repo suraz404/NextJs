25b2c26fccdbe1d928360cb8c2d1b847ce5039e4
Suraz Agrahari <surazagrahari1980@gmail.com>
2026-07-12 20:58:38 +0545

    setting up project structure

--- src/app/about/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>ABout</div>;
-};
-
-export default page;
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-linear-to-br from-blue-50 to-indigo-100 flex flex-col items-center justify-center p-6 ">
+      <div className="text-center">
+        <h1>Welcome to the Dashboard App</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
